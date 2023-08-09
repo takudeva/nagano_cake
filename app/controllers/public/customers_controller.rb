c290d2317de0b9efd3717c9946d926176e6c2b4d
takudeva <126333608+takudeva@users.noreply.github.com>
2023-08-09 08:47:19 +0000

    [Edit15]finish editing customers/edit view&controller and made customer/withdraw

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -9,4 +9,23 @@ class Public::CustomersController < ApplicationController
   def edit
     @customer = current_customer
   end
+
+  def update
+    @customer = current_customer
+    @customer.update(customer_params)
+    redirect_to my_page_path
+  end
+
+  def withdraw
+    @customer = current_customer
+    @customer.update(is_deleated: true)
+    reset_session
+    redirect_to "/"
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email)
+  end
+
 end
