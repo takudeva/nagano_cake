88712f6cacf00ccdbe4202269f6d485edda15225
takudeva <126333608+takudeva@users.noreply.github.com>
2023-08-12 01:24:33 +0000

    [Edit20]in making admin item MVC

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,13 +1,27 @@
 class Admin::ItemsController < ApplicationController
-  def index
-  end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_item_path(@item.id)
+  end
+
+  def index
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :genre_id, :price, :is_sold_out)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,13 @@
 class Item < ApplicationRecord
-  
+
   has_one_attached :image
-  
+
 end
+
+  def get_image(width,height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/mv.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [width,height]).processed
+  end
