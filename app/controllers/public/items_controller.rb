51af6bfc9ad0c9d264cbb1ccfa850171489e7f5d
takudeva <126333608+takudeva@users.noreply.github.com>
2023-08-13 23:31:12 +0000

    [Edit24]in making n items MVC

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,24 @@
 class Public::ItemsController < ApplicationController
   def index
+    @genres = Genre.all
+    @items = Item.page(params[:page])
   end
 
   def show
+    @genres = Genre.all
+    @item = Item.find(params[:id])
+    @cart_item = CartItem.new
   end
+
+  def create
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item.save
+    redirect_to cart_items_path
+  end
+
+  private
+  def cart_item_params
+    params.require(:cart_item).permit(:customer_id, :item_id, :amount)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
 
   belongs_to :genre
+  has_many :cart_items
 
   has_one_attached :image
 
