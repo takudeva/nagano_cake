965f0808c32358e8f046494d65325b8f9ba95ea9
takudeva <126333608+takudeva@users.noreply.github.com>
2023-08-17 23:39:54 +0000

    [Edit35]introduce enum in application.rb & made ja.yml & devise.ja.yml

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,12 @@
 class Order < ApplicationRecord
+  belongs_to :customer
+
+  enum method_of_payment: { credit: 0, bank: 1 }
+  enum status: {
+    waiting_for_payment: 0,
+    payment_confirmation: 1,
+    making: 2,
+    preparing_to_ship: 3,
+    shipped: 4
+  }
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,2 +1,11 @@
 class OrderItem < ApplicationRecord
+  belongs_to :order, :item
+
+  enum making_status: {
+    cannot_start_making: 0,
+    waiting_for_making: 1,
+    making: 2,
+    completion_of_making: 3
+  }
+  
 end
