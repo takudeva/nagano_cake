9d3ed44fbc3560c0716e3f03af6dd74edf0b3c73
takudeva <126333608+takudeva@users.noreply.github.com>
2023-08-18 09:41:06 +0000

    [Edit38]in making orders confirm

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -7,9 +7,14 @@ class Public::OrdersController < ApplicationController
   def confirm
     @order = Order.new(order_params)
     @shipping_address = ShippingAddress.find(params[:order][:shipping_address_id])
+    # binding.pry
     @order.postal_code = current_customer.postal_code
     @order.address = current_customer.address
     @order.name = current_customer.last_name + current_customer.first_name
+    @postage = 800
+    @cart_items = CartItem.all
+    @total = @cart_items.inject(0) { |sum, cart_item| sum + cart_item.subtotal }
+    @billing_amount = @postage + @total
   end
 
   def complete
@@ -23,6 +28,6 @@ class Public::OrdersController < ApplicationController
 
   private
   def order_params
-    params.require(:order).permit(:method_of_payment, :postal_code, :address, :name)
+    params.require(:order).permit(:postage, :method_of_payment, :postal_code, :address, :name)
   end
 end
