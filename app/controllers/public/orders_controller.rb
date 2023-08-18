6736e5d782568c4dae037d5109fdc393155cbedb
takudeva <126333608+takudeva@users.noreply.github.com>
2023-08-18 07:39:44 +0000

    [Edit37]made orders new

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,9 +1,15 @@
 class Public::OrdersController < ApplicationController
   def new
     @shipping_addresses = ShippingAddress.all
+    @order = Order.new
   end
 
   def confirm
+    @order = Order.new(order_params)
+    @shipping_address = ShippingAddress.find(params[:order][:shipping_address_id])
+    @order.postal_code = current_customer.postal_code
+    @order.address = current_customer.address
+    @order.name = current_customer.last_name + current_customer.first_name
   end
 
   def complete
@@ -13,5 +19,10 @@ class Public::OrdersController < ApplicationController
   end
 
   def show
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:method_of_payment, :postal_code, :address, :name)
   end
 end
