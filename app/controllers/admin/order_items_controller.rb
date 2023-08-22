939b8f2c4afce037cc6e9a5a07cbb9c006bec2e8
takudeva <126333608+takudeva@users.noreply.github.com>
2023-08-22 02:00:39 +0000

    [Edit46]in making admin order show status & making_status

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -1,3 +1,21 @@
 class Admin::OrderItemsController < ApplicationController
 
+  def making_status
+    @order = Order.find_by(params[:order_id])
+    @order_item = OrderItem.find(params[:id])
+    @order_items = @order.order_items.all
+    @order_item.update(order_item_params)
+    if @order_item.making_status == "making"
+      @order.update(status: "making")
+    elsif @order_items.each do |order_item|
+      making_status == "completion_of_making"
+      @order.status == "preparing_to_ship"
+    end
+    redirect_to admin_order_path(@order.id)
+  end
+
+  private
+  def order_item_params
+    params.require(:order_item).permit(:making_status)
+  end
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,21 @@
 class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
-    @order_items = OrderItem.all
+    @order_items = OrderItem.where(order_id: params[:id])
+  end
+
+  def order_status
+    @order = Order.find(params[:id])
+    @order_items = OrderItem.where(order_id: params[:id])
+    @order.update(order_params)
+    if @order.status == "payment_confirmation"
+      @order_items.update_all(making_status: "waiting_for_making")
+    end
+    redirect_to admin_order_path(@order.id)
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:status)
   end
 end
