00b8f99af5c3bf2dd879e87d098074bde31928c5
takudeva <126333608+takudeva@users.noreply.github.com>
2023-08-26 04:40:26 +0000

    [Edit55]change admin making_status

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -1,16 +1,18 @@
 class Admin::OrderItemsController < ApplicationController
 
   def making_status
-    @order = Order.find_by(params[:order_id])
-    @order_item = OrderItem.find(params[:id])
-    @order_items = @order.order_items.all
-    @order_item.update(order_item_params)
-    if @order_item.making_status == "making"
-      @order.update(status: "making")
-    elsif @order_items.all? { |order_item| order_item.making_status == "completion_of_making" }
-      @order.update(status: "preparing_to_ship")
+    order_item = OrderItem.find(params[:id])
+    order_id = order_item.order_id
+    order = Order.find(order_id)
+    # binding.pry
+    order_items = order.order_items.all
+    order_item.update(order_item_params)
+    if order_item.making_status == "making"
+      order.update(status: "making")
+    elsif order_items.all? { |order_item| order_item.making_status == "completion_of_making" }
+      order.update(status: "preparing_to_ship")
     end
-    redirect_to admin_order_path(@order.id)
+    redirect_to admin_order_path(order.id)
   end
 
   private
